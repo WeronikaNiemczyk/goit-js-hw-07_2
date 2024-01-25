7882f8d606eef280f17e234dce58a0ccfaec1731
Weronika Niemczyk <weronika.tlusciak@gmail.com>
2024-01-25 15:23:52 +0100

    task 2 - done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryPlace = document.querySelector(".gallery");
+
+const gallery = galleryItems
+  .map(
+    (item) =>
+      `<div class="gallery">
+      <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+    </div>`
+  )
+  .join("");
+
+galleryPlace.innerHTML = gallery;
+
+new SimpleLightbox(".gallery a", {
+  captions: true,
+
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
